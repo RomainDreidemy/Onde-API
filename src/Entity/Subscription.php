1c55986d0f4e725f465074f423142c03e330c3a1
romain.dreidemy <dreidemyromain@gmail.com>
2020-06-23 10:10:22 +0200

    Ajout de l'entité Subscription

--- src/Entity/Subscription.php
+++ src/Entity/Subscription.php
@@ -2,11 +2,17 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiProperty;
 use App\Repository\SubscriptionRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=SubscriptionRepository::class)
+ * @ApiResource(
+ *     collectionOperations={"get", "post"},
+ *     itemOperations={"get", "patch"}
+ * )
  */
 class Subscription
 {
@@ -20,12 +26,28 @@ class Subscription
     /**
      * @ORM\ManyToOne(targetEntity=User::class, inversedBy="subscriptions")
      * @ORM\JoinColumn(nullable=false)
+        @ApiProperty(
+            attributes={
+                "openapi_context"={
+                    "type"="relation",
+                    "example"="/api/users/1"
+                }
+            }
+        )
      */
     private $User;
 
     /**
      * @ORM\ManyToOne(targetEntity=Post::class, inversedBy="subscriptions")
      * @ORM\JoinColumn(nullable=false)
+        @ApiProperty(
+            attributes={
+                "openapi_context"={
+                    "type"="relation",
+                    "example"="/api/posts/1"
+                }
+            }
+        )
      */
     private $Post;
 
