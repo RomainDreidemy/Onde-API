3908aa8222352976d2d9ce654b657a8650e9c347
romain.dreidemy <dreidemyromain@gmail.com>
2020-07-03 15:14:55 +0200

    Envoie de mail lorsqu'un objectif est atteind

--- src/Controller/Api/Subscription/SubscriptionAddController.php
+++ src/Controller/Api/Subscription/SubscriptionAddController.php
@@ -4,37 +4,73 @@ namespace App\Controller\Api\Subscription;
 
 use App\Entity\Subscription;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SubscriptionAddController extends AbstractController
 {
     private $manager;
+    private $mailer;
 
-    public function __construct(EntityManagerInterface $entityManager, UserPasswordEncoderInterface $encoder)
+    public function __construct(EntityManagerInterface $entityManager, MailerInterface $mailer)
     {
         $this->manager = $entityManager;
+        $this->mailer = $mailer;
     }
 
-    public function __invoke(Subscription $data) : Subscription
+    public function __invoke(Subscription $data)
     {
-        $subscriptions = $data->getPost()->getSubscriptions();
         $goals = $data->getPost()->getPostGoals();
 
-        foreach ($goals as $goal){
-            if($goal->getDone() !== true){
-                if($goal->getNumber() <= (count($subscriptions) + 1)){
-                    $goal->setDone(true);
-                    $this->manager->persist($goal);
+        $alreadyExist = $this->manager->getRepository(Subscription::class)->findOneBy(['User' => $data->getUser(), 'Post' => $data->getPost()]);
 
-                    //Todo envoyer un mail
-                }
-            }
+//        return $this->json([count($subscriptions) + 1, ]);
+
+        if(!is_null($alreadyExist)){
+            return $this->json([
+                'error' => 'L\'utilisateur est déjà abonné à cette initiative.'
+            ], 500);
         }
 
         $this->manager->persist($data);
         $this->manager->flush();
+
+        $subscriptions = $data->getPost()->getSubscriptions();
+
+
+        foreach ($goals as $goal){
+            if($goal->getDone() !== true){
+
+                if($goal->getNumber() === count($subscriptions)){
+
+                    $goal->setDone(true);
+                    $this->manager->persist($goal);
+                    $this->manager->flush();
+
+                    foreach ($subscriptions as $subscription){
+                        $user = $subscription->getUser();
+
+                        $mail = (new TemplatedEmail())
+                            ->from('contact.onde.projet@gmail.com')
+                            ->to(new Address($user->getEmail()))
+                            ->subject('Onde - Objectif atteind : ' . $goal->getName())
+                            ->htmlTemplate('mailer/goal-done.html.twig')
+                            ->context([
+                                'user' => $user,
+                                'goal' => $goal
+                            ]);
+
+                        $this->mailer->send($mail);
+                    }
+                }
+            }
+        }
+
+
 
         return $data;
     }
