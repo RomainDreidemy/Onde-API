4265ece7adae0ddf553d2067f21348c76b128469
romain.dreidemy <dreidemyromain@gmail.com>
2020-07-03 10:17:06 +0200

    EasyAdmin : filtre Tags

--- src/Controller/Admin/TagsCrudController.php
+++ src/Controller/Admin/TagsCrudController.php
@@ -3,7 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Tags;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class TagsCrudController extends AbstractCrudController
 {
@@ -12,14 +17,29 @@ class TagsCrudController extends AbstractCrudController
         return Tags::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('name', 'Nom'),
+            CollectionField::new('Post', 'Nombre d\'utilisation')->hideOnForm(),
         ];
     }
-    */
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Initiative')
+            ->setEntityLabelInPlural('Initiatives')
+//            ->setEntityPermission('ROLE_ADMIN')
+            ->setPageTitle('index', 'Liste des %entity_label_plural%')
+            ;
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add('name')
+        ;
+    }
 }
