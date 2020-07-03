0d7812f2530a8c181a22380a7a41dcb7e70e6cec
romain.dreidemy <dreidemyromain@gmail.com>
2020-07-03 09:50:19 +0200

    Ajout de colonne et de filtre

--- src/Controller/Admin/PostCrudController.php
+++ src/Controller/Admin/PostCrudController.php
@@ -3,14 +3,18 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Post;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class PostCrudController extends AbstractCrudController
@@ -25,13 +29,36 @@ class PostCrudController extends AbstractCrudController
     {
         return [
             TextField::new('name', 'Nom'),
+            TextareaField::new('description', 'Description'),
             DateField::new('date_created', 'Date de création'),
             DateField::new('date_end', 'Date de fin'),
-            DateField::new('dateMeeting', 'Rendez-vous'),
+            DateTimeField::new('dateMeeting', 'Rendez-vous'),
             TextField::new('Department.name', 'Département')->onlyOnIndex(),
             AssociationField::new('likes', 'Goutte d\'eau')->onlyOnIndex(),
             AssociationField::new('subscriptions', 'Participant')->onlyOnIndex(),
             TextField::new('User.name', 'Créateur')->onlyOnIndex(),
+            BooleanField::new('validated', 'Validé'),
         ];
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Initiative')
+            ->setEntityLabelInPlural('Initiatives')
+//            ->setEntityPermission('ROLE_ADMIN')
+            ->setPageTitle('index', 'Liste des %entity_label_plural%')
+            ;
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add('name')
+            ->add('date_created')
+            ->add('date_end')
+            ->add('dateMeeting')
+            ->add('validated')
+            ;
     }
 }
