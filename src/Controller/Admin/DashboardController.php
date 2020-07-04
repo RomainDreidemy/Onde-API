713c81ee2dd93a6c12954995d606906cadcd9fc5
romain.dreidemy <dreidemyromain@gmail.com>
2020-07-04 11:50:18 +0200

    API : Ajout des icons

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -15,6 +15,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -24,6 +25,10 @@ class DashboardController extends AbstractDashboardController
     public function index(): Response
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY', null, 'Vous devez être connecté pour accéder à cette page');
+        $routeBuilder = $this->get(CrudUrlGenerator::class)->build();
+
+        return $this->redirect($routeBuilder->setController(UserCrudController::class)->generateUrl());
+
         return parent::index();
     }
 
@@ -37,21 +42,27 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
+
+
+
         if($this->isGranted('ROLE_ADMIN') || $this->isGranted('ROLE_PARTENAIRE')){
             yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
 
+            yield MenuItem::section('Api');
+            yield MenuItem::linktoRoute('Documentation', 'fas fa-book', 'api_entrypoint')->setLinkTarget('_blank');
+
             yield MenuItem::section('Initiatives');
-            yield MenuItem::linkToCrud('Initiatives', 'icon class', Post::class);
+            yield MenuItem::linkToCrud('Initiatives', 'far fa-newspaper', Post::class);
         }
 
         if($this->isGranted('ROLE_ADMIN')){
-            yield MenuItem::linkToCrud('Commentaires', 'icon class', Comment::class);
-            yield MenuItem::linkToCrud('Departements', 'icon class', Department::class);
-            yield MenuItem::linkToCrud('Tags', 'icon class', Tags::class);
-            yield MenuItem::linkToCrud('Goutte d\'eau', 'icon class', Like::class);
+            yield MenuItem::linkToCrud('Commentaires', 'far fa-comments', Comment::class);
+            yield MenuItem::linkToCrud('Departements', 'fas fa-map-marker-alt', Department::class);
+            yield MenuItem::linkToCrud('Tags', 'fas fa-tags', Tags::class);
+            yield MenuItem::linkToCrud('Goutte d\'eau', 'fa fa-tint', Like::class);
 
             yield MenuItem::section('----');
-            yield MenuItem::linkToCrud('Utilisateurs', 'icon class', User::class);
+            yield MenuItem::linkToCrud('Utilisateurs', 'fa fa-user', User::class);
         }
     }
 
