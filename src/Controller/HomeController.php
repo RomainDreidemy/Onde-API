5a3b653b8e5596aee1e78eb97dc97f76900d6ff0
romain.dreidemy <dreidemyromain@gmail.com>
2020-07-07 13:57:35 +0200

    Redirection de la home vers l'api

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -17,36 +17,6 @@ class HomeController extends AbstractController
      */
     public function index(EntityManagerInterface $entityManager, Request $request)
     {
-
-        $param = $request->query;
-
-        if(is_null(is_null($param->get('type')))){
-            return $this->json(['error' => 'Le paramètre type est requis'], 404);
-        }
-
-        $filtre = [];
-        $type = '';
-
-        if(!is_null($param->get('department'))){
-            $splitDepartment = explode(',', $param->get('department'));
-
-            foreach ($splitDepartment as $dep){
-                $filtre['department'][] = $dep;
-            }
-        }
-
-        if(!is_null($param->get('type'))){
-            $type = $param->get('type');
-        }
-
-        if(!is_null($param->get('limit'))){
-            $limit = $param->get('limit');
-        }
-//            dd($filtre);
-
-        $posts = $entityManager->getRepository(Post::class)->findByCritere($type, $filtre, ($limit ?? null));
-
-
-        return $this->json($posts, 200);
+        return $this->redirectToRoute('api_entrypoint');
     }
 }
